f2586a1de085c90310ac582be86ad00efdeae3e0
Rajorshi Roy Chowdhury <rajroyc.ca@gmail.com>
2026-01-18 15:07:19 -0500

    Initial Commit

---
+++ Tiny Wins/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tiny Wins
+//
+//  Created by Rajorshi Roy Chowdhury on 18/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tiny Wins/Tiny_WinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tiny_WinsApp.swift
+//  Tiny Wins
+//
+//  Created by Rajorshi Roy Chowdhury on 18/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Tiny_WinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
